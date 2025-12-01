a470dffde63cd15359238d52c88d0f8bcedf4c55
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-01 02:44:08 +0200

    refactor header - logo image & menus

--- src/components/Header copy.jsx
+++ src/components/Header copy.jsx
@@ -86,16 +86,10 @@ export default function Header() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a
-            href="#"
-            className="text-sm/6 font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
-          >
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Home
           </a>
-          <a
-            href="#"
-            className="text-sm/6 font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
-          >
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Recipe Catalog
           </a>
           <Popover className="relative">
@@ -156,16 +150,10 @@ export default function Header() {
           </Popover>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a
-            href="#"
-            className="text-sm/6 font-semibold text-gray-900 mr-10 hover:border-b-1 pl-2 pr-2"
-          >
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Register <span aria-hidden="true"></span>
           </a>
-          <a
-            href="#"
-            className="text-sm/6 font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
-          >
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Log in <span aria-hidden="true"></span>
           </a>
         </div>
@@ -180,7 +168,7 @@ export default function Header() {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img alt="" src={myImage} className="h-20 w-auto" />
+              <img alt="" src={myImage} className="h-8 w-auto" />
             </a>
             <button
               type="button"
@@ -194,22 +182,9 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Recipe Catalog
-                </a>
-
                 <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    More
+                    Product
                     <ChevronDownIcon
                       aria-hidden="true"
                       className="size-5 flex-none group-data-open:rotate-180"
@@ -228,14 +203,26 @@ export default function Header() {
                     ))}
                   </DisclosurePanel>
                 </Disclosure>
-              </div>
-              <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Register
+                  Features
                 </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
