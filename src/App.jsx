f20e6f0762e69aa7ea00589a2d89dcc234661d14
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-01 03:45:22 +0200

    refactor Tailwind Login & Reg pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Link, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Catalog from "./components/Catalog.jsx";
 import Register from "./components/Register.jsx";
-import Loing from "./components/Login.jsx";
+import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
 
 function App() {
@@ -11,11 +11,11 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/admin">
           <Route path="register" element={<Register />} />
-          <Route path="login" element={<Loing />} />
+          <Route path="login" element={<Login />} />
           <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
