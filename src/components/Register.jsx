2d802cca97e117599a0babdcde87bb29ee69970b
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-01 04:01:47 +0200

    take register data with new FormData()

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,12 @@
 import hatImage from "../assets/images/hat.png";
 
 export default function Register() {
+  const registerClickHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log(Object.fromEntries(formData));
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -16,7 +22,7 @@ export default function Register() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={registerClickHandler}>
             <div>
               <label
                 htmlFor="email"
