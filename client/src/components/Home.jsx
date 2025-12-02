cac3a443c24936b9db83618d171465eb87bbc914
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-03 00:24:44 +0200

    render recipes on home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
 export default function Home() {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3030/jsonstore/recipes")
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(Object.values(result));
+        setRecipes(Object.values(result));
+      })
+      .catch((err) => alert(err.message));
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl text-center font-bold ">All recipes</h1>
+      <h1 className="text-3xl text-center font-bold mt-10">All Recipes</h1>
+      <div className="border-t-4 border-amber-900 mt-4"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-6 w-4/5 mr-auto ml-auto">
+        {recipes.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="bg-white rounded-lg shadow-lg overflow-hidden"
+          >
+            <img
+              src={recipe.imageUrl}
+              alt={recipe.title}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold text-gray-800">
+                {recipe.title}
+              </h2>
+              <p className="text-gray-600 text-sm mb-4">{recipe.category}</p>
+              <div className="flex justify-between items-center">
+                <Link
+                  to={`/recipe/details/${recipe.id}`}
+                  className="text-amber-900 font-semibold hover:underline"
+                >
+                  Details
+                </Link>
+                <div className="flex items-center">
+                  <button className="text-amber-900 font-semibold hover:text-amber-700">
+                    👍 0
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- client/src/components/user/Register.jsx
+++ client/src/components/user/Register.jsx
@@ -113,7 +113,7 @@ export default function Register() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-amber-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-amber-900 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-amber-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-amber-900 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Register
               </button>
