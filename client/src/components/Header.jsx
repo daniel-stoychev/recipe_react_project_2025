4f8bae43cf52cf6d902665f6115081f8fac6adb2
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-02 21:46:03 +0200

    refactor Header to use NavLink and added isActive style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ import {
   PhoneIcon,
   PlayCircleIcon,
 } from "@heroicons/react/20/solid";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 const products = [
   {
@@ -89,18 +89,26 @@ export default function Header() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Link
+          <NavLink
             to="/"
-            className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+            className={({ isActive }) =>
+              `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                isActive ? "border-b-1" : ""
+              }`
+            }
           >
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/catalog"
-            className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+            className={({ isActive }) =>
+              `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                isActive ? "border-b-1" : ""
+              }`
+            }
           >
             Recipe Catalog
-          </Link>
+          </NavLink>
           <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-base font-semibold text-gray-900">
               More
@@ -160,39 +168,60 @@ export default function Header() {
         </PopoverGroup>
         {!isAuthenticated ? (
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link
+            <NavLink
               to="/admin/register"
-              className="text-base font-semibold text-gray-900 mr-10 hover:border-b-1 pl-2 pr-2"
+              className={({ isActive }) =>
+                `text-base font-semibold text-gray-900 mr-10 hover:border-b-1 pl-2 pr-2 ${
+                  isActive ? "border-b-1" : ""
+                }`
+              }
             >
               Register <span aria-hidden="true"></span>
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/admin/login"
-              className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+              className={({ isActive }) =>
+                `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                  isActive ? "border-b-1" : ""
+                }`
+              }
+              // className="text-base  font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
             >
               Log in <span aria-hidden="true"></span>
-            </Link>
+            </NavLink>
           </div>
         ) : (
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <p className="text-base font-semibold text-gray-900 pl-2 pr-2">
               {user.email}
             </p>
-            <Link
+            <NavLink
               to="/admin/recipe/create"
-              className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+              className={({ isActive }) =>
+                `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                  isActive ? "border-b-1" : ""
+                }`
+              }
             >
               Create Recipe <span aria-hidden="true"></span>
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/admin/profile"
-              className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+              className={({ isActive }) =>
+                `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                  isActive ? "border-b-1" : ""
+                }`
+              }
             >
               Profile <span aria-hidden="true"></span>
-            </Link>
+            </NavLink>
             <button
               onClick={() => onLogout()}
-              className="text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2"
+              className={({ isActive }) =>
+                `text-base font-semibold text-gray-900 hover:border-b-1 pl-2 pr-2 ${
+                  isActive ? "border-b-1" : ""
+                }`
+              }
             >
               Logout
             </button>
@@ -223,18 +252,18 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Link
+                <NavLink
                   to="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Home
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                   to="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Recipe Catalog
-                </Link>
+                </NavLink>
 
                 <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
@@ -259,18 +288,18 @@ export default function Header() {
                 </Disclosure>
               </div>
               <div className="py-6">
-                <Link
+                <NavLink
                   to="/admin/register"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Register
-                </Link>
-                <Link
+                </NavLink>
+                <NavLink
                   to="/admin/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Log in
-                </Link>
+                </NavLink>
               </div>
             </div>
           </div>
