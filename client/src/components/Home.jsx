7051e6327c61158e46aff50de7380595f9c3fbba
Daniel Stoychev <daniel.stoychev@siteground.com>
2025-12-03 15:23:47 +0200

    show there are no recipes on home if neccessary

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,39 +17,47 @@ export default function Home() {
     <>
       <h1 className="text-3xl text-center font-bold mt-10">All Recipes</h1>
       <div className="border-t-4 border-amber-700 mt-4"></div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-6 w-4/5 mr-auto ml-auto">
-        {recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            className="bg-white rounded-lg shadow-lg overflow-hidden"
-          >
-            <img
-              src={recipe.imageUrl}
-              alt={recipe.title}
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold text-gray-800">
-                {recipe.title}
-              </h2>
-              <p className="text-gray-600 text-sm mb-4">{recipe.category}</p>
-              <div className="flex justify-between items-center">
-                <Link
-                  to={`/recipe/details/${recipe.id}`}
-                  className="text-amber-900 font-semibold hover:underline"
-                >
-                  Details
-                </Link>
-                <div className="flex items-center">
-                  <button className="text-amber-900 font-semibold hover:text-amber-700">
-                    👍 0
-                  </button>
+      {recipes.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-6 w-4/5 mr-auto ml-auto">
+          {recipes.map((recipe) => (
+            <div
+              key={recipe.id}
+              className="bg-white rounded-lg shadow-lg overflow-hidden"
+            >
+              <img
+                src={recipe.imageUrl}
+                alt={recipe.title}
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {recipe.title}
+                </h2>
+                <p className="text-gray-600 text-sm mb-4">{recipe.category}</p>
+                <div className="flex justify-between items-center">
+                  <Link
+                    to={`/recipe/details/${recipe.id}`}
+                    className="text-amber-900 font-semibold hover:underline"
+                  >
+                    Details
+                  </Link>
+                  <div className="flex items-center">
+                    <button className="text-amber-900 font-semibold hover:text-amber-700">
+                      👍 0
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-3xl text-center font-bold mt-10">
+            There are no recipes!
+          </h1>
+        </div>
+      )}
     </>
   );
 }
