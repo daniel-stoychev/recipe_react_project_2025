b15cfd0d7bcfa0752a32a134a64dc723f24ee456
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-01-04 21:58:04 +0200

    update margin-top for <Spinner/>

--- client/src/components/ui/Spinner.jsx
+++ client/src/components/ui/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center mt-20">
       <div className="w-8 h-8 border-4 border-t-transparent border-amber-600 rounded-full animate-spin"></div>
     </div>
   );
